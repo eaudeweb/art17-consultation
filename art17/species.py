ec8833ea871989b7a4e2ebedd1060108da7193a2
Alex Morega <alex@grep.ro>
2013-08-27 19:14:47 +0300

    parametrize the view

--- art17/species.py
+++ art17/species.py
@@ -4,9 +4,10 @@ from art17 import models
 
 species = flask.Blueprint('species', __name__)
 
-@species.route('/specii')
-def species_view():
-    species = models.DataSpecies.query.filter_by(speciescode='1308').first()
+@species.route('/specii/<speciescode>')
+def species_view(speciescode):
+    species = models.DataSpecies.query.filter_by(
+        speciescode=speciescode).first()
     checklist = models.DataSpeciesCheckList.query.filter_by(
         natura_2000_code=species.speciescode,
         member_state=species.country)
