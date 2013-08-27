73fa81f47fbfa4b23d4aa70984af92d36210dd4f
Alex Morega <alex@grep.ro>
2013-08-27 17:56:31 +0300

    logging

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@
 
 
 def main():
+    import logging
+    logging.basicConfig()
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    logging.getLogger('werkzeug').setLevel(logging.INFO)
     from art17.app import manager
     manager.run()
 
