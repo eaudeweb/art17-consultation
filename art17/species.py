74ca6a78348a9e7c920712f397d170b9503d8ab5
Alex Morega <alex@grep.ro>
2013-08-28 16:56:41 +0300

    refactor _get_population_trend
    to use common _get_trend

--- art17/species.py
+++ art17/species.py
@@ -43,16 +43,14 @@ class SpeciesRecord(GenericRecord):
         else:
             return "%s-%s %s" % (min_size, max_size, unit)
 
-    def _get_population_trend(self, name=''):
-        period = getattr(self.row, 'population_trend%s_period' % name)
-        trend = getattr(self.row, 'population_trend%s' % name)
-        magnitude_min = getattr(self.row, 'population_trend%s_magnitude_min' % name)
-        magnitude_max = getattr(self.row, 'population_trend%s_magnitude_max' % name)
-        magnitude_ci = getattr(self.row, 'population_trend%s_magnitude_ci' % name)
-        method = getattr(self.row, 'population_trend%s_method' % name)
-        return "%s %s method=%s magnitude=(min=%s max=%s ci=%s)" % (
-            trend, self._split_period(period), method,
-            magnitude_min, magnitude_max, magnitude_ci)
+    def _get_population_trend(self, qualifier=''):
+        base_info = self._get_trend('population', qualifier)
+        magnitude_min = getattr(self.row, 'population_trend%s_magnitude_min' % qualifier)
+        magnitude_max = getattr(self.row, 'population_trend%s_magnitude_max' % qualifier)
+        magnitude_ci = getattr(self.row, 'population_trend%s_magnitude_ci' % qualifier)
+        method = getattr(self.row, 'population_trend%s_method' % qualifier)
+        return "%s method=%s magnitude=(min=%s max=%s ci=%s)" % (
+            base_info, method, magnitude_min, magnitude_max, magnitude_ci)
 
     def _get_habitat_quality(self):
         value = self.row.habitat_quality
