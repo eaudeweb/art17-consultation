195ad2d6533e2c73e0382c66a3d0c2b2dfc379db
Alex Morega <alex@grep.ro>
2013-08-28 15:44:18 +0300

    don't spam me with sql queries

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@
 def main():
     import logging
     logging.basicConfig()
-    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    #logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
     logging.getLogger('werkzeug').setLevel(logging.INFO)
     from art17.app import manager
     manager.run()
