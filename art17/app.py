f8345354557776866d8c32a2a1f367f4c03fa411
Alex Morega <alex@grep.ro>
2013-08-29 15:41:49 +0300

    cache busting for static files

--- art17/app.py
+++ art17/app.py
@@ -1,6 +1,7 @@
 import flask
 from flask.ext.script import Manager
 from werkzeug.local import LocalProxy
+from path import path
 
 models = LocalProxy(lambda: flask.current_app.extensions['art17_models'])
 
@@ -13,6 +14,17 @@ views = flask.Blueprint('views', __name__)
 @views.route('/')
 def home():
     return 'hello art17'
+
+
+@views.app_url_defaults
+def bust_cache(endpoint, values):
+    if endpoint == 'static':
+        filename = values['filename']
+        file_path = path(flask.current_app.static_folder) / filename
+        if file_path.exists():
+            mtime = file_path.stat().st_mtime
+            key = ('%x' % mtime)[-6:]
+            values['t'] = key
 
 
 def create_app():
