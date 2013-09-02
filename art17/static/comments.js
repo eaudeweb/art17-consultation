7adb1ce6e2df173ad215bd394920351bc31add07
Alex Morega <alex@grep.ro>
2013-09-02 14:55:05 +0300

    demo fields for habitat comment

--- art17/static/comments.js
+++ art17/static/comments.js
@@ -17,4 +17,21 @@ table.on('click', '.comment-cancel', function(evt) {
   $(evt.target).parents('table.records tr').remove();
 });
 
+
+table.on('click', '.comment-save', function(evt) {
+  var comment_tr = $(evt.target).parents('.comment');
+  var data = {};
+  _(comment_tr.find(':input')).forEach(function(el) {
+    var input = $(el);
+    data[input.attr('name')] = input.val();
+  });
+
+  var save_button = comment_tr.find('.comment-save');
+  save_button.attr('disabled', 'disabled');
+  save_button.empty().append("salvez ",
+                             $('<i>', {class: 'icon-spinner icon-spin'}));
+
+  console.log(data);
+});
+
 })();
