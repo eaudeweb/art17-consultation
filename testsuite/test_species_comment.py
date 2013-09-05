080d40d958813c1d57b7e60297f58a0643af42a5
Alex Morega <alex@grep.ro>
2013-09-05 12:51:07 +0300

    test that comment view returns 200

---
+++ testsuite/test_species_comment.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+@pytest.fixture
+def species_app():
+    import flask
+    import art17.app
+    from art17.species import species
+    app = flask.Flask('art17.app')
+    app.config['TESTING'] = True
+    app.register_blueprint(species)
+    from art17 import models as _models
+    app.extensions['art17_models'] = _models
+    _models.db.init_app(app)
+    with app.app_context():
+        _models.db.create_all()
+    return app
+
+
+def _create_species_record(species_app):
+    from art17.app import models
+    with species_app.app_context():
+        species = models.DataSpecies(species_id=1, speciescode='1234')
+        species.lu = models.LuHdSpecies(objectid=1, speciescode=1234)
+        record = models.DataSpeciesRegion(sr_id=1, sr_species=species)
+        record.lu = models.LuBiogeoreg(objectid=1)
+        models.db.session.add(record)
+        models.db.session.commit()
+
+
+def test_load_comments_view(species_app):
+    _create_species_record(species_app)
+    client = species_app.test_client()
+    resp = client.get('/specii/detalii/1/comentariu')
+    assert resp.status_code == 200
