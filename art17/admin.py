459a55ee9042c8f6b296a858c94aa7eec7197ccf
Alex Morega <alex@grep.ro>
2013-09-09 16:48:23 +0300

    protect the admin views

--- art17/admin.py
+++ art17/admin.py
@@ -1,6 +1,7 @@
 import flask
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqlamodel import ModelView as ModelView_base
+from art17 import auth
 from art17 import models
 
 admin = Admin(name="Art17")
@@ -12,6 +13,9 @@ class ModelView(ModelView_base):
         super(ModelView, self).__init__(model, models.db.session)
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+    def is_accessible(self):
+        return auth.admin_permission.can()
 
 
 admin.add_view(ModelView(models.DataSpeciesRegion))
