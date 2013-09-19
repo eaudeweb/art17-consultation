3875b1cf51a7ca1a8b37dd4abd79401d48345eff
Alex Morega <alex@grep.ro>
2013-09-19 16:21:51 +0300

    run sql script by hand

---
+++ alembic/versions/44fc139f3b69_habitattype_ro.py
@@ -0,0 +1,20 @@
+revision = '44fc139f3b69'
+down_revision = '5413be0df64'
+
+from alembic import op
+import sqlalchemy as sa
+from path import path
+
+MANUAL_SCRIPTS = (path(__file__).abspath().parent.parent /
+                  'manual_oracle_scripts')
+
+
+def upgrade():
+    script_path = MANUAL_SCRIPTS / '4_lu_habitattype_codes_nume.sql'
+    for statement in script_path.text(encoding='utf-8').split(';\n'):
+        if statement.strip():
+            op.execute(statement)
+
+
+def downgrade():
+    raise NotImplementedError
