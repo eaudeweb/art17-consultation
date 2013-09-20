c96985ad60888718e6514f462c1c267c37c19adb
Alex Morega <alex@grep.ro>
2013-09-20 15:03:28 +0300

    move conclusion status code to indexpage.js

--- art17/static/indexpage.js
+++ art17/static/indexpage.js
@@ -121,4 +121,33 @@ $('body').on('click', '.showmap', function(evt) {
 });
 
 
+var menu_items = [
+  {value: 'new', label: "nou"},
+  {value: 'approved', label: "aprobat"},
+  {value: 'rejected', label: "refuzat"}
+];
+
+
+_($('.records-conclusionstatus [name=status]')).forEach(function(el) {
+  var hidden_input = $(el);
+  var select = $('<select name="status">');
+  _(menu_items).forEach(function(item) {
+    var option = $('<option>', {value: item.value});
+    option.text(item.label);
+    if(hidden_input.val() == item.value) {
+      option.attr('selected', true);
+    }
+    select.append(option);
+  });
+  hidden_input.replaceWith(select);
+});
+
+
+$('.records-conclusionstatus').change(function(evt) {
+  var form = $(this);
+  var select = $(evt.target);
+  select.hide();
+  form.append('...').submit();
+});
+
 })();
