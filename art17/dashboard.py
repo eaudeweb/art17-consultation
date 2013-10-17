fa4ded8b807a602d22ccadac9a5989a627a44987
Alex Morega <alex@grep.ro>
2013-10-17 17:48:56 +0300

    show conclusion counts

--- art17/dashboard.py
+++ art17/dashboard.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import flask
+from art17 import models
 
 
 dashboard = flask.Blueprint('dashboard', __name__)
@@ -6,4 +8,24 @@ dashboard = flask.Blueprint('dashboard', __name__)
 
 @dashboard.route('/')
 def index():
-    return flask.render_template('dashboard/index.html')
+    session = models.db.session
+    DH = models.DataHabitat
+    DHR = models.DataHabitattypeRegion
+    DHC = models.DataHabitattypeConclusion
+
+    habitat_regions = {}
+    for row in session.query(DHR.habitat_id, DHR.region):
+        key = (int(row.habitat_id), row.region)
+        habitat_regions[key] = True
+
+    habitat_conclusion_count = defaultdict(int)
+    for row in session.query(DHC.habitat_id, DHC.region):
+        key = (int(row.habitat_id), row.region)
+        habitat_conclusion_count[key] += 1
+
+    return flask.render_template('dashboard/index.html', **{
+        'bioreg_list': models.LuBiogeoreg.query.all(),
+        'habitat_list': DH.query.join(DH.lu).all(),
+        'habitat_regions': habitat_regions,
+        'habitat_conclusion_count': dict(habitat_conclusion_count),
+    })
