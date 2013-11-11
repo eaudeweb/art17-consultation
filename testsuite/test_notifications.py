3a0e0937800d0660554adfc90c8f6028db5e74b9
Alex Eftimie <alex@eftimie.ro>
2013-11-11 18:32:12 +0200

    Prettify mail test

--- testsuite/test_notifications.py
+++ testsuite/test_notifications.py
@@ -1,6 +1,6 @@
 import pytest
-from flask.ext.mail import email_dispatched
 from art17 import models
+from art17.notifications import mail
 from test_species import _create_species_record
 
 
@@ -14,12 +14,9 @@ def _create_user_record(app):
 def test_add_species_region_comment(notifications_app):
     _create_species_record(notifications_app, comment=True)
     _create_user_record(notifications_app)
-    sent_emails = []
-    def email_dispatched_handler(message, app):
-       sent_emails.extend(message.recipients)
-    email_dispatched.connect(email_dispatched_handler)
     client = notifications_app.test_client()
-    resp = client.post('/specii/detalii/2/comentarii',
+    with mail.record_messages() as outbox:
+        resp = client.post('/specii/detalii/2/comentarii',
                        data={'range.surface_area': '50',
                              'range.method': '1',
                              'population.method': '1',
@@ -29,5 +26,6 @@ def test_add_species_region_comment(notifications_app):
                              'habitat.quality': '2',
                              'habitat.quality_explanation': 'foo explanation',
                              'habitat.area_suitable': 1000})
-    assert resp.status_code == 200
-    assert 'user@example.com' in sent_emails
+        assert resp.status_code == 200
+        assert len(outbox) == 1
+        assert 'user@example.com' in outbox[0].recipients
