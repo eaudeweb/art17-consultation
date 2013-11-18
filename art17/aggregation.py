0716889f40929c67646c3a499ea3e77f50f152d5
Alex Morega <alex@grep.ro>
2013-11-18 16:08:30 +0200

    execute queries the on database with primary data

--- art17/aggregation.py
+++ art17/aggregation.py
@@ -11,3 +11,18 @@ def inject_home_url():
 @aggregation.route('/')
 def home():
     return flask.render_template('aggregation/home.html')
+
+
+@aggregation.route('/agregare')
+def aggregate():
+    q = "SELECT SYS_CONTEXT('USERENV', 'SESSION_USER') FROM DUAL"
+    return flask.render_template('aggregation/aggregate.html', **{
+        'result': execute_on_primary(q).scalar(),
+    })
+
+
+def execute_on_primary(query):
+    from art17.models import db
+    app = flask.current_app
+    aggregation_engine = db.get_engine(app, 'primary')
+    return db.session.execute(query, bind=aggregation_engine)
