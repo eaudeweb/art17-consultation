8401382a20670a6a7eba21489edfd4f04db03d4a
Alex Eftimie <alex@eftimie.ro>
2013-12-03 18:04:49 +0200

    Refactor comment-saved.html use new generic properties.

--- art17/common.py
+++ art17/common.py
@@ -254,6 +254,7 @@ class RecordView(IndexMixin, flask.views.View):
                                         self.get_next_url()
         self.template_ctx['blueprint'] = self.blueprint
         self.template_ctx['record_id'] = self.record.id
+        self.template_ctx['subject'] = self.record.subject
 
         if flask.request.method == 'POST' and self.form.validate():
             self.flatten_commentform(self.form.data, self.object)
