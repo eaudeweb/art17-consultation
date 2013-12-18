887a159a6a7088b576eb18a150ee773f95dadcc2
Alex Morega <alex@grep.ro>
2013-12-18 14:56:22 +0200

    inline the oracle sql script for LU_GRUP_SPECIE

--- alembic/versions/d9d32baf6d5_manual.py
+++ alembic/versions/d9d32baf6d5_manual.py
@@ -3,19 +3,33 @@ down_revision = '000000000000'
 
 from alembic import op
 import sqlalchemy as sa
-from path import path
-
-MANUAL_SCRIPTS = (path(__file__).abspath().parent.parent /
-                  'manual_oracle_scripts')
-
+from sqlalchemy.sql import table, column
+from sqlalchemy.dialects.oracle import NVARCHAR2
 
 
 def upgrade():
-    for script_name in ['1_lu_grup_specie.sql']:
-        script_path = MANUAL_SCRIPTS / script_name
-        for statement in script_path.text(encoding='utf-8').split(';\n'):
-            if statement.strip():
-                op.execute(statement)
+    op.create_table(
+        'LU_GRUP_SPECIE',
+        sa.Column('OID', sa.Integer(), nullable=False),
+        sa.Column('CODE', NVARCHAR2(255), nullable=True),
+        sa.Column('DESCRIPTION', NVARCHAR2(255), nullable=True),
+    )
+
+    lu_grup_specie = table(
+        'lu_grup_specie',
+        column('oid', sa.Integer),
+        column('code', NVARCHAR2),
+        column('description', NVARCHAR2),
+    )
+
+    op.bulk_insert(lu_grup_specie, [
+        {'oid': 1, 'code': "A", 'description': "Amfibieni"},
+        {'oid': 2, 'code': "F", 'description': "Pesti"},
+        {'oid': 3, 'code': "M", 'description': "Mamifere"},
+        {'oid': 4, 'code': "P", 'description': "Plante"},
+        {'oid': 5, 'code': "R", 'description': "Reptile"},
+        {'oid': 6, 'code': "I", 'description': "Nevertebrate"},
+    ])
 
 
 def downgrade():
