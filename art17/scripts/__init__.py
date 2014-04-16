5604b8767cea65c757266c0f64fb3b70a4fa4a2f
Mihai Tabara <tabara.mihai@gmail.com>
2014-04-16 16:42:51 +0300

    aggregation: add script to dump data about species regions - range ref values.

--- art17/scripts/__init__.py
+++ art17/scripts/__init__.py
@@ -2,4 +2,4 @@ from flask.ext.script import Manager
 
 exporter = Manager()
 
-import art17.scripts.species
+import art17.scripts.reference_values
--- art17/scripts/reference_values.py
+++ art17/scripts/reference_values.py
@@ -3,7 +3,8 @@ from art17.scripts import exporter
 
 DATASET = 1
 
-def get_DataSpeciesRegion_magnitudes():
+@exporter.command
+def species_magnitude():
     header = ('Cod specie', 'Nume', 'Bioregiune', 'Magn. min scurt',
               'Magn. max scurt', 'Magn. min lung', 'Magn. max lung')
     print ','.join(header)
@@ -22,9 +23,7 @@ def get_DataSpeciesRegion_magnitudes():
             row = [
                 sp.code,
                 sp.name or sp.alternative_speciesname or '',
-                unicode(sr.id),
                 sr.region,
-                unicode(sr.cons_dataset_id),
                 unicode(sr.range_trend_magnitude_min or ''),
                 unicode(sr.range_trend_magnitude_max or ''),
                 unicode(sr.range_trend_long_magnitude_min or ''),
@@ -32,6 +31,32 @@ def get_DataSpeciesRegion_magnitudes():
             ]
             print ','.join(row)
 
+
 @exporter.command
-def species():
-    get_DataSpeciesRegion_magnitudes()
+def species_range_reference():
+    header = ('Cod specie', 'Nume', 'Bioregiune',
+              'Val. min areal favorabil referinta',
+              'Val de la care arealul devine nefavorabil - inadecvat',
+              'Valoare de la care arealul devine nefavorabil - rau',
+              'Valoare de la care arealul devine necunoscut')
+
+    print ','.join(header)
+
+    data_species = models.DataSpecies.query.all()
+    for sp in data_species:
+        data_species_regions = (
+            sp.regions
+            .filter_by(
+                cons_dataset_id=DATASET,
+                cons_role='assessment',
+            )
+            .order_by(models.DataSpeciesRegion.region)
+        )
+        for sr in data_species_regions:
+            row = [
+                sp.code,
+                sp.name or sp.alternative_speciesname or '',
+                sr.region,
+            ]
+            row.extend([''] * 4)
+            print ','.join(row)
