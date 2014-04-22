b802f6c23e4d1cf47c54522130c581b3c902159b
Mihai Tabara <tabara.mihai@gmail.com>
2014-04-22 17:40:34 +0300

    aggregation: group export scrits by sub groups.

--- art17/scripts/reference_values.py
+++ art17/scripts/reference_values.py
@@ -9,32 +9,40 @@ def species_magnitude():
     header = ('Cod specie', 'Nume', 'Bioregiune', 'Magn. min scurt',
               'Magn. max scurt', 'Magn. min lung', 'Magn. max lung')
 
+    groups = models.LuGrupSpecie.query.all()
     columns = []
-    data_species = models.DataSpecies.query.all()
-    for sp in data_species:
-        data_species_regions = (
-            sp.regions
-            .filter_by(
-                cons_dataset_id=DATASET,
-                cons_role='assessment',
-            )
-            .order_by(models.DataSpeciesRegion.region)
+    for group in groups:
+        columns.append([group.description])
+        data_species = (
+            models.DataSpecies.query
+            .join(models.DataSpecies.lu)
+            .filter(models.LuHdSpecies.group_code==group.code)
+            .order_by(models.DataSpecies.code)
         )
-        for sr in data_species_regions:
-            name = None
-            if sp.lu:
-                name = sp.lu.display_name
-            name = name or ''
-            row = [
-                sp.code,
-                name,
-                sr.region,
-                unicode(sr.range_trend_magnitude_min or ''),
-                unicode(sr.range_trend_magnitude_max or ''),
-                unicode(sr.range_trend_long_magnitude_min or ''),
-                unicode(sr.range_trend_long_magnitude_max or ''),
-            ]
-            columns.append(row)
+        for sp in data_species:
+            data_species_regions = (
+                sp.regions
+                .filter_by(
+                    cons_dataset_id=DATASET,
+                    cons_role='assessment',
+                )
+                .order_by(models.DataSpeciesRegion.region)
+            )
+            for sr in data_species_regions:
+                name = None
+                if sp.lu:
+                    name = sp.lu.display_name
+                name = name or ''
+                row = [
+                    sp.code,
+                    name,
+                    sr.region,
+                    unicode(sr.range_trend_magnitude_min or ''),
+                    unicode(sr.range_trend_magnitude_max or ''),
+                    unicode(sr.range_trend_long_magnitude_min or ''),
+                    unicode(sr.range_trend_long_magnitude_max or ''),
+                ]
+                columns.append(row)
 
     ret = generate_csv(header, columns)
     print ret
@@ -48,29 +56,37 @@ def species_range_reference():
               'Valoare de la care arealul devine nefavorabil - rau',
               'Valoare de la care arealul devine necunoscut')
 
+    groups = models.LuGrupSpecie.query.all()
     columns = []
-    data_species = models.DataSpecies.query.all()
-    for sp in data_species:
-        data_species_regions = (
-            sp.regions
-            .filter_by(
-                cons_dataset_id=DATASET,
-                cons_role='assessment',
-            )
-            .order_by(models.DataSpeciesRegion.region)
+    for group in groups:
+        columns.append([group.description])
+        data_species = (
+            models.DataSpecies.query
+            .join(models.DataSpecies.lu)
+            .filter(models.LuHdSpecies.group_code==group.code)
+            .order_by(models.DataSpecies.code)
         )
-        for sr in data_species_regions:
-            name = None
-            if sp.lu:
-                name = sp.lu.display_name
-            name = name or ''
-            row = [
-                sp.code,
-                name,
-                sr.region,
-            ]
-            row.extend([''] * 4)
-            columns.append(row)
+        for sp in data_species:
+            data_species_regions = (
+                sp.regions
+                .filter_by(
+                    cons_dataset_id=DATASET,
+                    cons_role='assessment',
+                )
+                .order_by(models.DataSpeciesRegion.region)
+            )
+            for sr in data_species_regions:
+                name = None
+                if sp.lu:
+                    name = sp.lu.display_name
+                name = name or ''
+                row = [
+                    sp.code,
+                    name,
+                    sr.region,
+                ]
+                row.extend([''] * 4)
+                columns.append(row)
 
     ret = generate_csv(header, columns)
     print ret
