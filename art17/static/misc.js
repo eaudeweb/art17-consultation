ce264e6b2b73b8389d05cc9fff7b3ba088658017
Mihai Tabara <tabara.mihai@gmail.com>
2014-05-07 17:16:36 +0300

    aggregation: add js to sync range, population and habitat conclusions on main page.

--- art17/static/misc.js
+++ art17/static/misc.js
@@ -2,4 +2,36 @@ $(function() {
     $('select.semaphore').on('change', function() {
         $(this).attr('data-color', $(this).val());
     }).change();
+
+    $('select[name="range.conclusion.value"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="rangelink.conclusion.value"]').val(text);
+    }).change();
+
+    $('select[name="range.conclusion.trend"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="rangelink.conclusion.trend"]').val(text);
+    }).change();
+
+
+    $('select[name="population.conclusion.value"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="populationlink.conclusion.value"]').val(text);
+    }).change();
+
+    $('select[name="population.conclusion.trend"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="populationlink.conclusion.trend"]').val(text);
+    }).change();
+
+
+    $('select[name="habitat.conclusion.value"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="habitatlink.conclusion.value"]').val(text);
+    }).change();
+
+    $('select[name="habitat.conclusion.trend"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="habitatlink.conclusion.trend"]').val(text);
+    }).change();
 });
