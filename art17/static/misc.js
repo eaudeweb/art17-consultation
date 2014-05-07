c88747df01ccdb92b58fb5796f7608b9a3e181c3
Mihai Tabara <tabara.mihai@gmail.com>
2014-05-07 18:14:25 +0300

    aggregation: add js to sync population on natura 2k page.

--- art17/static/misc.js
+++ art17/static/misc.js
@@ -7,7 +7,6 @@ $(function() {
         var text = $(this).find('option:selected').text();
         $('input[name="rangelink.conclusion.value"]').val(text);
     }).change();
-
     $('select[name="range.conclusion.trend"]').on('change', function() {
         var text = $(this).find('option:selected').text();
         $('input[name="rangelink.conclusion.trend"]').val(text);
@@ -18,7 +17,6 @@ $(function() {
         var text = $(this).find('option:selected').text();
         $('input[name="populationlink.conclusion.value"]').val(text);
     }).change();
-
     $('select[name="population.conclusion.trend"]').on('change', function() {
         var text = $(this).find('option:selected').text();
         $('input[name="populationlink.conclusion.trend"]').val(text);
@@ -29,9 +27,37 @@ $(function() {
         var text = $(this).find('option:selected').text();
         $('input[name="habitatlink.conclusion.value"]').val(text);
     }).change();
-
     $('select[name="habitat.conclusion.trend"]').on('change', function() {
         var text = $(this).find('option:selected').text();
         $('input[name="habitatlink.conclusion.trend"]').val(text);
     }).change();
+
+
+    $('select[name="population.size.population.unit"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="populationlink.size.population.unit"]').val(text);
+    }).change();
+    $('input[name="population.size.population.min"]').on('change', function() {
+        var value = $(this).val();
+        $('input[name="populationlink.size.population.min"]').val(value);
+    }).change();
+    $('input[name="population.size.population.max"]').on('change', function() {
+        var value = $(this).val();
+        $('input[name="populationlink.size.population.max"]').val(value);
+    }).change();
+
+
+    $('select[name="population.size.population_alt.unit"]').on('change', function() {
+        var text = $(this).find('option:selected').text();
+        $('input[name="populationlink.size.population_alt.unit"]').val(text);
+    }).change();
+    $('input[name="population.size.population_alt.min"]').on('change', function() {
+        var value = $(this).val();
+        $('input[name="populationlink.size.population_alt.min"]').val(value);
+    }).change();
+    $('input[name="population.size.population_alt.max"]').on('change', function() {
+        var value = $(this).val();
+        $('input[name="populationlink.size.population_alt.max"]').val(value);
+    }).change();
+
 });
