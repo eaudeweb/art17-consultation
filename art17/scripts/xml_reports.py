ce2757d1e2f30d7afb1bb78ea3366690e145235c
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2014-05-09 15:11:05 +0300

    export species checklist in xml format

--- art17/scripts/xml_reports.py
+++ art17/scripts/xml_reports.py
@@ -16,6 +16,32 @@ def xml_species(filename=None):
 
 
 @exporter.command
+def xml_species_checklist(filename=None):
+    species = models.db.session.query(
+                    models.DataSpeciesCheckList).filter_by(
+                            member_state='RO')
+
+    species_as_dict = {}
+    for sp in species:
+        try:
+            species_as_dict[sp.code]['regions'].append(sp)
+        except KeyError:
+            species_as_dict[sp.code] = {
+                        'info': sp,
+                        'regions': [sp],
+                    }
+
+    data = flask.render_template(
+                'xml_export/species_checklist.html',
+                **{'species_dict': species_as_dict}
+            )
+
+    if filename:
+        with open(filename, 'w') as file_out:
+            file_out.write(data.encode('utf-8'))
+
+
+@exporter.command
 def xml_habitats(filename=None):
     habitats = models.db.session.query(models.DataHabitat)
     data = flask.render_template(
