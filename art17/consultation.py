4f30fc860d137cb8e4b4d1e38110c94053c6ba18
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-07-31 18:17:47 +0300

    [homepage] include number of species/habitats with reports.

--- art17/consultation.py
+++ art17/consultation.py
@@ -26,17 +26,19 @@ def get_reports_data(overview):
         'final': {section: 0 for section in sections},
         'notfinal': {section: 0 for section in sections},
     }
+    record_ids = set([])
     for record, data in overview.iteritems():
         target = 'final' if 'final_record' in data else 'notfinal'
         reports_data[target]['reports'] += 1
         reports_data[target]['comments'] += data['count']
         reports_data[target]['replies'] += data['with_reply']
+        record_ids.add(record[0])
     reports_data['all'] = {
         section:
         reports_data['final'][section] + reports_data['notfinal'][section]
         for section in sections
     }
-    return reports_data
+    return reports_data, len(record_ids)
 
 
 @consultation.app_context_processor
@@ -56,19 +58,21 @@ def home():
         habitat_dataset
         .get_subject_region_overview_consultation(flask.g.identity.id)
     )
-    habitat_details = get_reports_data(habitat_overview)
+    habitat_details, habitat_count = get_reports_data(habitat_overview)
 
     species_dataset = get_species_dataset()
     species_overview = (
         species_dataset
         .get_subject_region_overview_consultation(flask.g.identity.id)
     )
-    species_details = get_reports_data(species_overview)
+    species_details, species_count = get_reports_data(species_overview)
 
     return flask.render_template(
         'consultation/home.html', **{
             'habitat_details': habitat_details,
+            'habitat_count': habitat_count,
             'species_details': species_details,
+            'species_count': species_count,
         })
 
 
