accd4811bc63f2235aade5d3436ba966cbbc7f23
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-11-11 16:33:01 +0200

    [testing] Fix reports urls in tests.

--- testsuite/test_reports.py
+++ testsuite/test_reports.py
@@ -21,104 +21,104 @@ def test_report_home(aggregation_app):
 def test_report_1(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/missing')
+    resp = client.get('/raport/1/raport1')
     assert resp.status_code == 200
 
 
+def test_report_2(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport2')
+    assert resp.status_code == 200
+
+
+def test_report_3(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport3')
+    assert resp.status_code == 200
+
+
+def test_report_4(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport4')
+    assert resp.status_code == 200
+
+
+def test_report_5(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport5')
+    assert resp.status_code == 200
+
+
+def test_report_6(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport6')
+    assert resp.status_code == 200
+
+
+def test_report_7(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport7')
+    assert resp.status_code == 200
+
+
+@pytest.mark.xfail(raises=OperationalError)
 def test_report_8(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/validation')
+    resp = client.get('/raport/1/raport8')
+    assert resp.status_code == 200
+
+
+def test_report_9(aggregation_app):
+    _create_dataset(aggregation_app)
+    client = aggregation_app.test_client()
+    resp = client.get('/raport/1/raport9')
     assert resp.status_code == 200
 
 
 def test_report_10(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/bioreg_annex')
+    resp = client.get('/raport/1/raport10')
     assert resp.status_code == 200
 
 
 def test_report_11(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/conservation_status')
+    resp = client.get('/raport/1/raport11')
     assert resp.status_code == 200
 
 
 def test_report_12(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/bioreg_global')
+    resp = client.get('/raport/1/raport12')
     assert resp.status_code == 200
 
 
 def test_report_13(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/13')
+    resp = client.get('/raport/1/raport13')
     assert resp.status_code == 200
 
 
 def test_report_14(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/14')
+    resp = client.get('/raport/1/raport14')
     assert resp.status_code == 200
 
 
-@pytest.mark.xfail(raises=OperationalError)
 def test_report_15(aggregation_app):
     _create_dataset(aggregation_app)
     client = aggregation_app.test_client()
-    resp = client.get('/raport/1/15')
-    assert resp.status_code == 200
-
-
-def test_report_16(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/16')
-    assert resp.status_code == 200
-
-
-def test_report_17(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/17')
-    assert resp.status_code == 200
-
-
-def test_report_18(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/pressures1')
-    assert resp.status_code == 200
-
-
-def test_report_20(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/measures')
-    assert resp.status_code == 200
-
-
-def test_report_21(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/measures_effects')
-    assert resp.status_code == 200
-
-
-def test_report_22(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/quality')
-    assert resp.status_code == 200
-
-
-def test_report_23(aggregation_app):
-    _create_dataset(aggregation_app)
-    client = aggregation_app.test_client()
-    resp = client.get('/raport/1/statistics23')
+    resp = client.get('/raport/1/raport15')
     assert resp.status_code == 200
