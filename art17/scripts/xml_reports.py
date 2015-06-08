a5c62c0ad9cdc0db6459459a9f389589c373ba97
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2015-06-08 18:34:35 +0300

    [xml exports] Remove duplicates in checklist export.

--- art17/scripts/xml_reports.py
+++ art17/scripts/xml_reports.py
@@ -3,14 +3,19 @@ from art17 import models
 from art17.scripts import exporter, DEFAULT_DATASET_ID
 
 
+def get_current_checklist_id():
+    cnf = models.Config.query.filter_by(id='REPORTING_ID').first()
+    return int(cnf.value) if cnf and cnf.value else None
+
+
 @exporter.command
 def xml_species(filename=None, dataset_id=None):
     dataset_id = dataset_id or DEFAULT_DATASET_ID
     species = models.db.session.query(models.DataSpecies)
     data = flask.render_template(
-                'xml_export/species.html',
-                **{'species_list': species, 'dataset_id': dataset_id}
-            )
+        'xml_export/species.html',
+        **{'species_list': species, 'dataset_id': dataset_id}
+    )
     if filename:
         with open(filename, 'w') as file_out:
             file_out.write(data.encode('utf-8'))
@@ -19,24 +24,25 @@ def xml_species(filename=None, dataset_id=None):
 
 @exporter.command
 def xml_species_checklist(filename=None):
-    species = models.db.session.query(
-                    models.DataSpeciesCheckList).filter_by(
-                            member_state='RO')
-
+    checklist_id = get_current_checklist_id()
+    species = models.db.session.query(models.DataSpeciesCheckList).filter_by(
+        member_state='RO',
+        dataset_id=checklist_id,
+    )
     species_as_dict = {}
     for sp in species:
         try:
             species_as_dict[sp.code]['regions'].append(sp)
         except KeyError:
             species_as_dict[sp.code] = {
-                        'info': sp,
-                        'regions': [sp],
-                    }
+                'info': sp,
+                'regions': [sp],
+            }
 
     data = flask.render_template(
-                'xml_export/species_checklist.html',
-                **{'species_dict': species_as_dict}
-            )
+        'xml_export/species_checklist.html',
+        **{'species_dict': species_as_dict}
+    )
 
     if filename:
         with open(filename, 'w') as file_out:
@@ -49,9 +55,9 @@ def xml_habitats(filename=None, dataset_id=None):
     dataset_id = dataset_id or DEFAULT_DATASET_ID
     habitats = models.db.session.query(models.DataHabitat)
     data = flask.render_template(
-                'xml_export/habitats.html',
-                **{'habitats': habitats, 'dataset_id': dataset_id}
-            )
+        'xml_export/habitats.html',
+        **{'habitats': habitats, 'dataset_id': dataset_id}
+    )
     if filename:
         with open(filename, 'w') as file_out:
             file_out.write(data.encode('utf-8'))
@@ -60,24 +66,25 @@ def xml_habitats(filename=None, dataset_id=None):
 
 @exporter.command
 def xml_habitats_checklist(filename=None):
-    habitats = models.db.session.query(
-                    models.DataHabitatsCheckList).filter_by(
-                            member_state='RO')
-
+    checklist_id = get_current_checklist_id()
+    habitats = models.db.session.query(models.DataHabitatsCheckList).filter_by(
+        member_state='RO',
+        dataset_id=checklist_id,
+    )
     habitats_as_dict = {}
     for hb in habitats:
         try:
             habitats_as_dict[hb.code]['regions'].append(hb)
         except KeyError:
             habitats_as_dict[hb.code] = {
-                        'info': hb,
-                        'regions': [hb],
-                    }
+                'info': hb,
+                'regions': [hb],
+            }
 
     data = flask.render_template(
-                'xml_export/habitats_checklist.html',
-                **{'habitats_dict': habitats_as_dict}
-            )
+        'xml_export/habitats_checklist.html',
+        **{'habitats_dict': habitats_as_dict}
+    )
 
     if filename:
         with open(filename, 'w') as file_out:
